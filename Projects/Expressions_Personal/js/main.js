d4bb81ea855beb4873874b91dec71f962bc9e82c
Marco Ledesma <marco.j.ledesma@gmail.com>
2013-04-17 20:42:15 -0400

    ADding the Expressions Personal Folder with a few files

---
+++ Projects/Expressions_Personal/js/main.js
@@ -0,0 +1,14 @@
+/*
+	Name: Marco Ledesma
+	
+*/
+
+
+var startProgram = function() {
+	alert("This personal program of mine will calculate how much money in gas you will spend in a year");
+	var mpg = prompt("Enter your car's mpg (miles per gallon)");
+	var estimatedFuelPrice = prompt("Enter the current price for today's gas (i.e. if gas is 3.95/gal, then enter 3.95)");
+	var milesPerYear = prompt("Finally, enter the number of miles you may drive in a year");
+
+
+}
