ffd5e52c084e5e8f7d48c1cb360e7d28e17138d6
Marco Ledesma <marco.j.ledesma@gmail.com>
2013-04-17 20:55:38 -0400

    Added more lines

--- Projects/Expressions_Personal/js/main.js
+++ Projects/Expressions_Personal/js/main.js
@@ -9,10 +9,24 @@ var startProgram = function() {
 	var mpg = prompt("Enter your car's mpg (miles per gallon)");
 	var estimatedFuelPrice = prompt("Enter the current price for today's gas (i.e. if gas is 3.95/gal, then enter 3.95)");
 	var milesPerYear = prompt("Finally, enter the number of miles you may drive in a year");
+	var error = false;
 
 	mpg = parseInt(mpg);
-	estimatedFuelPrice = parseInt(estimatedFuelPrice);
+	estimatedFuelPrice = parseFloat(estimatedFuelPrice);
 	milesPerYear = parseInt(milesPerYear);
+
+	//error checking
+	if (isNaN(mpg) || isNaN(estimatedFuelPrice) || isNaN(milesPerYear))
+	{
+		var conf = confirm("Whoah! One of your entries was not a number. Please do not enter characters, only numbers. Click OK to restart this program");
+		if (conf == true) {
+			startProgram();
+			return;
+		}
+		else {
+			return;
+		}
+	}
 
 	console.log(mpg);
 	console.log(estimatedFuelPrice);
