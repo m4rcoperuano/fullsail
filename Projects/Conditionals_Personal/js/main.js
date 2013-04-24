008d00398bfb7123dbe9f5504eac359759eaf9dd
Marco Ledesma <marco.j.ledesma@gmail.com>
2013-04-23 21:59:31 -0400

    first code commit

--- Projects/Conditionals_Personal/js/main.js
+++ Projects/Conditionals_Personal/js/main.js
@@ -10,4 +10,32 @@ function DoProject()
 	alert("Here we go again..");
 	var name = prompt("What's your name?");
 
+	var areYouSalary = prompt("Are you salary? Enter Y or N");
+	while (areYouSalary != "Y" || areYouSalary != "N")
+	{
+		areYouSalary = prompt("Are you salary? Enter Y or N!");
+	}
+
+	if (areYouSalary == "Y")
+	{
+
+	}
+	else 
+	{
+		alert("Alright, assuming your salary..");
+		var howMuchHourly = prompt("How much do you make per hour? Enter a number.");
+		var howManyHours = prompt("How many hours do you make?");
+
+		var hourlyNumber = parseInt(howMuchHourly);
+		var howManyHoursNumber = parseInt(howManyHours);
+		while (isNaN(hourlyNumber) || isNaN(howManyHoursNumber))
+		{
+			howMuchHourly = prompt("How much do you make per hour? Enter a number.");
+			howManyHours = prompt("How many hours do you make?");
+
+			hourlyNumber = parseInt(howMuchHourly);
+			howManyHoursNumber = parseInt(howManyHours);
+		}
+	}
+	
 }
