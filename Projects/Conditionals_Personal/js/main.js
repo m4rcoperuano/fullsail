7608ec6a844be938ddfbc69562a15432d4e7391f
Marco Ledesma <marco.j.ledesma@gmail.com>
2013-04-23 21:51:07 -0400

    Adding all conditional files

---
+++ Projects/Conditionals_Personal/js/main.js
@@ -0,0 +1,13 @@
+/*
+	Name: Marco Ledesma
+	Date: April 23, 2013
+	Assignment: Conditional Personal
+	Description: How much money you make a year
+*/
+
+function DoProject() 
+{
+	alert("Here we go again..");
+	var name = prompt("What's your name?");
+
+}
