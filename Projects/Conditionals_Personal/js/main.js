170e69ef17099e9c893c14c67005c45d7894e7e8
Marco Ledesma <marco.j.ledesma@gmail.com>
2013-04-23 22:14:28 -0400

    first code commit

--- Projects/Conditionals_Personal/js/main.js
+++ Projects/Conditionals_Personal/js/main.js
@@ -5,13 +5,18 @@
 	Description: How much money you make a year
 */
 
+function log(message) {
+	console.log(message);
+}
+
 function DoProject() 
 {
 	alert("Here we go again..");
+	alert("So this program will calulate how much money (gross) you make a year. So. Lets do this....");
 	var name = prompt("What's your name?");
 
 	var areYouSalary = prompt("Are you salary? Enter Y or N");
-	while (areYouSalary != "Y" || areYouSalary != "N")
+	while (areYouSalary.toLowerCase() != "y" && areYouSalary.toLowerCase() != "n")
 	{
 		areYouSalary = prompt("Are you salary? Enter Y or N!");
 	}
@@ -22,19 +27,31 @@ function DoProject()
 	}
 	else 
 	{
-		alert("Alright, assuming your salary..");
+		alert("Alright, assuming your hourly..");
 		var howMuchHourly = prompt("How much do you make per hour? Enter a number.");
-		var howManyHours = prompt("How many hours do you make?");
+		var howManyHours = prompt("How many hours do you work a week? Enter a number.");
 
 		var hourlyNumber = parseInt(howMuchHourly);
 		var howManyHoursNumber = parseInt(howManyHours);
-		while (isNaN(hourlyNumber) || isNaN(howManyHoursNumber))
-		{
-			howMuchHourly = prompt("How much do you make per hour? Enter a number.");
-			howManyHours = prompt("How many hours do you make?");
 
+		while (isNaN(hourlyNumber))
+		{
+			howMuchHourly = prompt("How much do you make per hour? Enter a number!");
 			hourlyNumber = parseInt(howMuchHourly);
+		}
+		while (isNaN(howManyHoursNumber))
+		{
+			howManyHours = prompt("How many hours do you make? Enter a number!");
 			howManyHoursNumber = parseInt(howManyHours);
+		}
+
+		var result = hourlyNumber * (howManyHoursNumber * 52);
+
+		alert("Please check the console for the results");
+		log("Hello " + name + "! Here are your results..");
+		if (result <= 25000)
+		{
+			log("Wow I'd hate to have your job. You only make $" + result + " a year!");
 		}
 	}
 	
