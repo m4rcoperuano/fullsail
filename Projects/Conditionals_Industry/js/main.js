3b3671636be35bfb0ced99ed63bca8becb50e34d
Marco Ledesma <marco.j.ledesma@gmail.com>
2013-04-25 19:04:24 -0400

    Adding comments to Conditionals Industry

--- Projects/Conditionals_Industry/js/main.js
+++ Projects/Conditionals_Industry/js/main.js
@@ -41,25 +41,32 @@ var Validation = function() {
 
 function CalculateCenter()
 {
+	//introduce the program
 	alert("This program will find the coordinates of the center of a web page..");
 	alert("Assuming you know the width and height of the browser. Ideally, you would get this "+
 		"using code (like pageX and pageY)");
 
+	//ask for the user's name
 	var name = prompt("What is your name?");
-	name = Validation.JString(name);
+	name = Validation.JString(name); //validate
 	var widthOfBrowser = prompt("Please enter the width of the browser in pixels (approx)");
-	widthOfBrowser = Validation.Integer(widthOfBrowser);
-	var heightOfBrowser = prompt("Please enter the height of the browser");
-	heightOfBrowser = Validation.Integer(heightOfBrowser);
+	//get width of the browser
+	widthOfBrowser = Validation.Integer(widthOfBrowser);//validate width of browser
+	var heightOfBrowser = prompt("Please enter the height of the browser"); //get height of the browser
+	heightOfBrowser = Validation.Integer(heightOfBrowser); //validate the height
 
+	//check the console
 	alert("Please check the console for the result");
-	console.log("Awesome " + name);
+	console.log("Awesome " + name); //log the user's name
+	//log the width of the browser and the height
 	console.log("So using this width: "+widthOfBrowser + ", and this height: " +heightOfBrowser +"...");
 
+	//calculate the result, put it in an object
 	var result = {};
 	result.xCoord = widthOfBrowser/2;
 	result.yCoord = heightOfBrowser/2;
 
+	//log the x and y coordinates using the result object
 	console.log("We have the X Coordinate as " + result.xCoord + "px's from the left, and the Y Coordinate as "+result.yCoord+"px's from the top.");
 
 }	
