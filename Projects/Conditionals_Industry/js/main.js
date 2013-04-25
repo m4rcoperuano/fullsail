8e4e02aa63694afad9889a795b2b9659fbf8aab9
Marco Ledesma <marco.j.ledesma@gmail.com>
2013-04-25 18:57:30 -0400

    finished Conditional Industry

--- Projects/Conditionals_Industry/js/main.js
+++ Projects/Conditionals_Industry/js/main.js
@@ -9,20 +9,21 @@ var Validation = function() {
 	return {
 		Integer: function(value) {
 			value = parseInt(value);
-			var flag = false;
+			var flag = (isNaN(value)) ? false : true;
 			while (!flag)
 			{
 				value = prompt("Please enter a number!");
 				value = parseInt(value);
 				flag = (isNaN(value)) ? false : true;
-
 			}
+			return value;
 		},
 		JString: function (value) {
 			while (value == "")
 			{
 				value = prompt("Please enter some text!");				
 			}
+			return value;
 		}
 	}
 }();
@@ -33,12 +34,21 @@ function CalculateCenter()
 	alert("Assuming you know the width and height of the browser. Ideally, you would get this "+
 		"using code (like pageX and pageY)");
 
-	var name = promp("What is your name?");
+	var name = prompt("What is your name?");
 	name = Validation.JString(name);
 	var widthOfBrowser = prompt("Please enter the width of the browser in pixels (approx)");
 	widthOfBrowser = Validation.Integer(widthOfBrowser);
 	var heightOfBrowser = prompt("Please enter the height of the browser");
 	heightOfBrowser = Validation.Integer(heightOfBrowser);
 
+	alert("Please check the console for the result");
+	console.log("Awesome " + name);
+	console.log("So using this width: "+widthOfBrowser + ", and this height: " +heightOfBrowser +"...");
+
+	var result = {};
+	result.xCoord = widthOfBrowser/2;
+	result.yCoord = heightOfBrowser/2;
+
+	console.log("We have the X Coordinate as " + result.xCoord + "px's from the left, and the Y Coordinate as "+result.yCoord+"px from the top.");
 
 }	
