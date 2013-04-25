25adfdad38710c5e36e523b82f0ae93bb0348514
Marco Ledesma <marco.j.ledesma@gmail.com>
2013-04-25 19:19:52 -0400

    STarting Conditionals Wacky

--- Projects/Conditionals_Wacky/js/main.js
+++ Projects/Conditionals_Wacky/js/main.js
@@ -1,3 +1,14 @@
 /*
-	
+	Name: Marco Ledesma
+	Date: April 25, 2013
+	Assignment: Conditional Wacky
+	Description: 
 */
+
+
+function Begin()
+{
+	alert("Hello, welcome to something wacky");
+	
+
+}
