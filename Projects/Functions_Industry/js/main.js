1b88b54a0d1998e251ed3309d0ccaea00340649a
Marco Ledesma <marco.j.ledesma@gmail.com>
2013-04-29 22:35:51 -0400

    Initial functions commit

---
+++ Projects/Functions_Industry/js/main.js
@@ -0,0 +1,6 @@
+/*
+	Name: Marco Ledesma
+	Date: 4/29/13
+	Assignment: Functions_Industry
+	Description:
+*/
---
+++ Projects/Functions_Wacky/js/main.js
@@ -0,0 +1,6 @@
+/*
+	Name: Marco Ledesma
+	Date: 4/18/2013
+	Assignment: Functions_Wacky
+	Description: 
+*/
