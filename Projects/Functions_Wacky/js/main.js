f922a0e188598f256ff60f1bb1ff94da06bc7008
Marco Ledesma <marco.j.ledesma@gmail.com>
2013-04-30 00:11:23 -0400

    Finished assignment Functions_Wacky

--- Projects/Functions_Wacky/js/main.js
+++ Projects/Functions_Wacky/js/main.js
@@ -2,7 +2,8 @@
 	Name: Marco Ledesma
 	Date: 4/29/2013
 	Assignment: Functions_Wacky
-	Description: Random!
+	Description: This program will tell you what kind of person you are based on
+	arbitrary values!
 */
 
 function Wacky() {
@@ -10,11 +11,24 @@ function Wacky() {
 	var year = parseInt(prompt("What year were you born?"));
 	var month = parseInt(prompt("What month were you born on?"));
 	var randomNum = parseInt(prompt("Enter your favorite number"));
-	calculations(year, month, randomNum)
+	alert("check the console for results");
+	calculations(year, month, randomNum);
 }
 
 function calculations(year, month, rand)
 {
-	var result = (((year + month)/rand)*year- (month*year*rand))/950
-	console.log(result);
+	var result = (((year + month)/rand)*year- (month*year*rand))/950;	
+
+	if (result <= -1000)
+	{		
+		console.log("You are one negative person!");
+	}
+	else if (result < 0)
+	{
+		console.log("Hmm..seems like your a little negative.");
+	}
+	else if (result >= 0)
+	{
+		console.log("You seem like a positive fella!");		
+	}
 }
